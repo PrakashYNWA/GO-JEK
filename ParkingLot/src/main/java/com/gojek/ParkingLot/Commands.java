9b752aa6427cfb64765d6dd2ad42d947f90e1c06
Prakash <zuperman8692@gmail.com>
2018-05-14 04:34:53 +0800

    Fully implemented populateCommandsHashMap method

--- ParkingLot/src/main/java/com/gojek/ParkingLot/Commands.java
+++ ParkingLot/src/main/java/com/gojek/ParkingLot/Commands.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Commands {
 	public Map<String, Method> commandsMap;
 
@@ -16,7 +17,14 @@ public class Commands {
         }
     }
     private void populateCommandsHashMap() throws NoSuchMethodException {
-    	
+    	 
+    	commandsMap.put("create_parking_lot", Carpark.class.getMethod("create_parking_lot", String.class));
+         commandsMap.put("park", Carpark.class.getMethod("park", String.class, String.class));
+         commandsMap.put("leave", Carpark.class.getMethod("leave", String.class));
+         commandsMap.put("status", Carpark.class.getMethod("status"));
+         commandsMap.put("registration_numbers_for_cars_with_colour", Carpark.class.getMethod("registration_numbers_for_cars_with_colour", String.class));
+         commandsMap.put("slot_numbers_for_cars_with_colour", Carpark.class.getMethod("slot_numbers_for_cars_with_colour", String.class));
+         commandsMap.put("slot_number_for_registration_number", Carpark.class.getMethod("slot_number_for_registration_number", String.class));
     }
 
 }
