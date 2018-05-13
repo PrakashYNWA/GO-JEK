ee916f17a7a97a248a38b7f77c1f4ddd72acd1de
Prakash <zuperman8692@gmail.com>
2018-05-14 04:15:31 +0800

    Empty Commands class, to insert input commands, and test case created

---
+++ ParkingLot/src/test/java/com/gojek/ParkingLot/CommandsTest.java
@@ -0,0 +1,14 @@
+package com.gojek.ParkingLot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommandsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
