ffa89fc75d7faf26b3730556a2b4c6c31d3e367e
Prakash <zuperman8692@gmail.com>
2018-05-14 01:42:54 +0800

    Created empty create_parking_lot method with corresponding test case

--- ParkingLot/src/main/java/com/gojek/ParkingLot/Carpark.java
+++ ParkingLot/src/main/java/com/gojek/ParkingLot/Carpark.java
@@ -1,5 +1,22 @@
 package com.gojek.ParkingLot;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Carpark {
+	
+	int MAX_SIZE = 0;
+	// List of available slots
+    ArrayList<Integer> list_of_available_slots;
+    // Mapping of slot no to car
+    Map<String, Car> slot_to_car;
+    // Mapping of reg no to slot no
+    Map<String, String> reg_to_slot;
+    // Mapping of colour to list of reg_no with that colour
+    Map<String, ArrayList<String>> colour_to_reg;
+    
+    public void create_parking_lot(String size){
+    	
+    }
 
 }
--- ParkingLot/src/test/java/com/gojek/ParkingLot/CarparkTest.java
+++ ParkingLot/src/test/java/com/gojek/ParkingLot/CarparkTest.java
@@ -2,13 +2,35 @@ package com.gojek.ParkingLot;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+
 public class CarparkTest {
+	
+	Carpark carpark = new Carpark();
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void create_parking_lot() throws Exception {
+		carpark.create_parking_lot("6");
+        assertEquals(6, carpark.MAX_SIZE);
+        assertEquals(6, carpark.list_of_available_slots.size());
+        assertTrue("createdaparkinglotwith6slots".equalsIgnoreCase(outContent.toString().trim().replace(" ", "")));
+        
+    }
 
 }
