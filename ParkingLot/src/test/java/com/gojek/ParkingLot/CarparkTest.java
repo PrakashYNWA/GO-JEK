89bcd80d153bb32cac7d6f2f06c32ad56646158f
Prakash <zuperman8692@gmail.com>
2018-05-14 01:34:01 +0800

    Created Carpark class and CarparkTest JUnit class

---
+++ ParkingLot/src/test/java/com/gojek/ParkingLot/CarparkTest.java
@@ -0,0 +1,14 @@
+package com.gojek.ParkingLot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CarparkTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
