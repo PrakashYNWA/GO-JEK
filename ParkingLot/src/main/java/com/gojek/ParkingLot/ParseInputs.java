7aeaa56cc29fa8b088cfa25d0ce2eb1748f5d740
Prakash <zuperman8692@gmail.com>
2018-05-14 04:50:48 +0800

    Implemented ParseInputs test case

--- ParkingLot/src/main/java/com/gojek/ParkingLot/ParseInputs.java
+++ ParkingLot/src/main/java/com/gojek/ParkingLot/ParseInputs.java
@@ -1,5 +1,16 @@
 package com.gojek.ParkingLot;
 
 public class ParseInputs {
+	
+	Commands commands;
+    static Carpark carpark;
+    public ParseInputs() {
+        commands = new Commands();
+        carpark = new Carpark();
+    }
+    
+    public void parseTextInput(String inputString) {
+    	
+    }
 
 }
--- ParkingLot/src/test/java/com/gojek/ParkingLot/ParseInputsTest.java
+++ ParkingLot/src/test/java/com/gojek/ParkingLot/ParseInputsTest.java
@@ -2,13 +2,34 @@ package com.gojek.ParkingLot;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParseInputsTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	ParseInputs parseInputs = new ParseInputs();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+    @Test
+    public void parseTextInput() throws Exception {
+    	parseInputs.parseTextInput("test");
+        assertEquals("Invalidinput", outContent.toString().trim().replace(" ", ""));
+        parseInputs.parseTextInput("leave3");
+        assertEquals("Invalidinput\nParkinglothasnotbeencreated", outContent.toString().trim().replace(" ", ""));
+        parseInputs.parseTextInput("create_parking_lot6");
+        assertTrue("createdaparkinglotwith6slots".equalsIgnoreCase(outContent.toString().trim().replace(" ", "")));
+    }
 
 }
