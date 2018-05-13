bd2bdbf52d6de9ade02aa568c23ff9df9b58febe
Prakash <zuperman8692@gmail.com>
2018-05-14 01:52:56 +0800

    Fully implemented create_parking_lot method

--- ParkingLot/src/main/java/com/gojek/ParkingLot/Carpark.java
+++ ParkingLot/src/main/java/com/gojek/ParkingLot/Carpark.java
@@ -1,7 +1,9 @@
 package com.gojek.ParkingLot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
 
 public class Carpark {
 	
@@ -16,7 +18,24 @@ public class Carpark {
     Map<String, ArrayList<String>> colour_to_reg;
     
     public void create_parking_lot(String size){
-    	
+    	try{
+			this.MAX_SIZE = Integer.parseInt(size);
+		}
+		catch (Exception e){
+			System.out.println("Please input a valid number for the carpark size");
+			System.out.println();
+		}
+		
+		this.list_of_available_slots = new ArrayList<Integer>();
+		for (int i = 0; i < this.MAX_SIZE; i++){
+			this.list_of_available_slots.add(i+1);
+		}
+		this.slot_to_car = new HashMap<String,Car> ();
+		this.reg_to_slot = new HashMap<String, String>();
+        this.colour_to_reg = new HashMap<String, ArrayList<String>>();
+
+		System.out.println("Created a parking lot with " +size + " slots");
+		System.out.println();
     }
 
 }
