f900b2a2342bb7c31cc7e8a2dc63e63b0e26d7d2
Prakash <zuperman8692@gmail.com>
2018-05-14 01:23:07 +0800

    Created class for Car object

---
+++ ParkingLot/src/main/java/com/gojek/ParkingLot/Car.java
@@ -0,0 +1,14 @@
+package com.gojek.ParkingLot;
+
+public class Car {
+	
+	String regNo;
+	String colour;
+	
+	public Car(String number, String colour){
+		this.regNo = number;
+		this.colour = colour;
+		
+	}
+
+}
