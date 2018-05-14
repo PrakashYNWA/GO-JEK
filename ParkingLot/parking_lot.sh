f0f82375a0aee9c640302c12b6bbc192fad14b2a
Prakash <zuperman8692@gmail.com>
2018-05-14 22:14:28 +0800

    Changed input file name

--- ParkingLot/parking_lot.sh
+++ ParkingLot/parking_lot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-chmod +x
+chmod 777 parking_lot.sh
 
 PASSED=$1
 if  [[ -f $PASSED ]]
 then 
 
 	mvn package
-	java -jar target/ParkingLot-0.0.1-SNAPSHOT.jar input_file.txt
+	java -jar target/ParkingLot-0.0.1-SNAPSHOT.jar file_inputs.txt
 	
 	
 else
